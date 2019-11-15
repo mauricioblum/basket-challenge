506fea317b21f2a2ef16beb321bcdc8e4894fe48
Mauricio Blum <mauriciocblum@gmail.com>
2019-11-15 16:27:16 -0300

    Initial app config

---
+++ .eslintrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+  },
+  extends: ['airbnb', 'prettier', 'prettier/react'],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+    __DEV__: true,
+  },
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaFeatures: {
+      jsx: true,
+    },
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['react', 'prettier'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.jsx', '.js'] }],
+    'import/prefer-default-export': 'off',
+    'no-param-reassign': 'off',
+    camelcase: 'off',
+    'prefer-destructuring': 'off',
+    'no-console': ['off', { allow: ['tron'] }],
+    'react/jsx-props-no-spreading': [
+      'off',
+      { html: 'ignore', exceptions: ['Route', 'Component', 'Layout', 'img'] },
+    ],
+  }
+};
---
+++ src/pages/ProductList/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+// import { Container } from './styles';
+
+export default function ProductList() {
+  return <div>Product List</div>;
+}
